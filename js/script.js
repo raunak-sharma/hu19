608cbd46a88bd0f74550f1914a3fc3d68d06d1c3
raunak-sharma <raunak.sharma.cse20@heritageit.edu.in>
2019-03-09 17:41:00 +0530

    timer added

--- js/script.js
+++ js/script.js
@@ -1,31 +1,32 @@
 (function () {
 
+    // to hide the loader
     window.addEventListener("load", function () {
-        const loader = document.querySelector(".loader");
-        loader.className += " hidden";
+        setTimeout(function () {
+            const loader = document.querySelector(".loader");
+            loader.className += " hidden";
+        }, 1000);
     });
-    // timestamp for Monday, 1 April 2019 03:30:00 = 1554089400
 
     document.addEventListener('DOMContentLoaded', function () {
 
-        var eDisplayMoment = document.getElementById('displayMoment');
-        eDisplayMoment.innerHTML = moment("20190401", "YYYYMMDD").fromNow();
-
-        /*
+        // timestamp for Monday, 1 April 2019 03:30:00 = 1554089400
         let eventTime = 1554089400;
-        let eventNow = Date.now();
-        console.log(eventNow);
+        let eventNow = Math.floor(new Date().getTime()/1000.0);
         let diffTime = eventTime - eventNow;
         let duration = moment.duration(diffTime*1000, 'milliseconds');
         let interval = 1000;
 
         setInterval(function(){
             duration = moment.duration(duration - interval, 'milliseconds');
-            document.getElementsByClassName('countdown')[0].innerHTML = duration.days() +
-                "<p>" + duration.hours() + ":" + duration.minutes() + ":" + duration.seconds() + "</p>";
-        }, interval);
-        */
+            let days = duration.days(), hrs = duration.hours(), mins = duration.minutes(), secs = duration.seconds();
 
+            document.getElementsByClassName('countdown')[0].innerHTML = "<p>" + days + " DAYS </br> "
+                + hrs + " Hours</br> " + mins + " Mins</br> " + secs + " Secs</p>";
+        }, 1000);
+
+
+        // navbar toggle
         let hamburger = document.querySelector(".hamburger");
         let navbar = document.querySelector("nav");
         let navitems = document.querySelector("nav .row");
