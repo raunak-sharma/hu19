ca92368426ad4ac367303f46bd0c7a98894151bd
raunak-sharma <raunak.sharma.cse20@heritageit.edu.in>
2019-03-10 11:46:21 +0530

    added footer

--- js/script.js
+++ js/script.js
@@ -10,6 +10,26 @@
 
     document.addEventListener('DOMContentLoaded', function () {
 
+        /* ANIMATION DEFINITIONS */
+
+            // initializing scroll magic
+            let controller = new ScrollMagic.Controller();
+
+            let aboutC = new ScrollMagic.Scene({
+                triggerElement : '#details-c'
+            })
+            .setClassToggle('#details-c', 'fade-in')
+            .addTo(controller);
+
+            let aboutT = new ScrollMagic.Scene({
+                triggerElement : '#details-t'
+            })
+            .setClassToggle('#details-t', 'fade-in')
+            .addTo(controller);
+
+
+        /* END OF ANIMATION DEFINITIONS */
+
         // timestamp for Monday, 1 April 2019 03:30:00 = 1554089400
         let eventTime = 1554089400;
         let eventNow = Math.floor(new Date().getTime()/1000.0);
@@ -22,7 +42,10 @@
             let days = duration.days(), hrs = duration.hours(), mins = duration.minutes(), secs = duration.seconds();
 
             document.getElementsByClassName('countdown')[0].innerHTML =
-            "<p style=\"font-family:'lmonoULight'\"><span style='font-size: 8vh;'>" + days + "</span> Days </br> <span style='font-size: 8vh;'>"
+            "<p style=\"font-family:'lmonoULight'\">"
+                + "<span style=\"font-size: 4vh\">Starts in </span></br>"
+                + "<span style='font-size: 8vh;'>"
+                + days + "</span> Days </br> <span style='font-size: 8vh;'>"
                 + hrs + "</span> Hours</br> <span style='font-size: 8vh;'>"
                 + mins + "</span> Minutes</br> <span style='font-size: 8vh;'>"
                 + secs + "</span> Seconds</p>";
@@ -39,7 +62,6 @@
             navitems.classList.toggle("nav-items-hidden");
             navitems.classList.toggle("nav-items-show");
         });
-
 
     });
 
